559016c82f0eebe6207b50fbe67f0769a2804920
Matias Carballo <andres.carballo-r@mail.escuelaing.edu.co>
2026-08-19 18:28:57 -0500

    Add Prolog program for flight and city data

---
+++ LAB01/ejercicio01.pl
@@ -0,0 +1,24 @@
+ciudad(bogota).
+ciudad(medellin).
+ciudad(cali).
+ciudad(cartagena).
+ciudad(manizales).
+ciudad(barranquilla).
+ciudad(pasto).
+ciudad(monteria).
+
+vuelo(bogota, medellin).
+vuelo(medellin, cartagena).
+vuelo(cali, bogota).
+vuelo(bogota, cartagena).
+vuelo(manizales, cartagena).
+vuelo(medellin, barranquilla).
+vuelo(pasto, bogota).
+vuelo(bogota, pasto).
+
+escala(X, Y) :- vuelo(X, Z), vuelo(Z, Y).
+
+viaje(X, Y) :- vuelo(X, Y).
+viaje(X, Y) :- escala(X, Y).
+
+destinos(X, Y) :- vuelo(X, Y).
